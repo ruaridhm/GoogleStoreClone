3b720ed8cdc9c320ff8719f194a0758174ace84e
ruaridhm <ruaridh.mcguckin@gmail.com>
2020-10-29 12:57:15 +0000

    css fixes for footer

--- src/Components/Footer/FooterMain.js
+++ src/Components/Footer/FooterMain.js
@@ -5,41 +5,38 @@ const FooterMain = () => {
     <div className='footer-main'>
       <div className='footer-main-left'></div>
       <div className='footer-main-right'>
-        <div className='footer-items-1'>
-          <ul className='footer-list-1'>
-            <li className='footer-item'>
-              <a href='/'>Placing an order</a>
-            </li>
-            <li className='footer-item'>
-              <a href='/'>Delivery options</a>
-            </li>
-            <li className='footer-item'>
-              <a href='/'>Tracking a package</a>
-            </li>
-            <li className='footer-item'>
-              <a href='/'>Country availability</a>
-            </li>
-            <li className='footer-item'>
-              <a href='/'>Repairs</a>
-            </li>
-            <li className='footer-item'>
-              <a href='/'>Find a nest pro</a>
-            </li>
-            <li className='footer-item'>
-              <a href='/'>Nest legal</a>
-            </li>
-            <li className='footer-item'>
-              <a href='/'>Nest privacy</a>
-            </li>
-          </ul>
-        </div>
-        <div className='footer-items-2'>
-          <ul className='footer-list-2'>
-            <li className='footer-item'>
-              <a href='/'>Device recycling</a>
-            </li>
-          </ul>
-        </div>
+        <ul className='footer-list-1'>
+          <li className='footer-item'>
+            <a href='/'>Placing an order</a>
+          </li>
+          <li className='footer-item'>
+            <a href='/'>Delivery options</a>
+          </li>
+          <li className='footer-item'>
+            <a href='/'>Tracking a package</a>
+          </li>
+          <li className='footer-item'>
+            <a href='/'>Country availability</a>
+          </li>
+          <li className='footer-item'>
+            <a href='/'>Repairs</a>
+          </li>
+          <li className='footer-item'>
+            <a href='/'>Find a Nest Pro</a>
+          </li>
+          <li className='footer-item'>
+            <a href='/'>Nest legal</a>
+          </li>
+          <li className='footer-item'>
+            <a href='/'>Nest privacy</a>
+          </li>
+        </ul>
+
+        <ul className='footer-list-2'>
+          <li className='footer-item'>
+            <a href='/'>Device recycling</a>
+          </li>
+        </ul>
       </div>
     </div>
   );
